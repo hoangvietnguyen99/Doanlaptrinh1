62d72227c16316e3943b80ac1d307e9ab4f063e4
hoangvietnguyen99 <38027261+hoangvietnguyen99@users.noreply.github.com>
2018-04-27 16:07:17 +0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -100,25 +100,47 @@ void getHobby(FILE* file, wchar_t a[10][100], int &soST)
 	}
 }
 
-void layDuLieu(FILE* file, SV &a, int &soST)
+void layDuLieu(FILE* file, SV &a, int &soST, int mail)
 {
 	getInfo(file, a.MSSV);
 	getInfo(file, a.HVT);
 	getInfo(file, a.KHOA);
 	laySo(file, a.KHOAS);
-	getInfo(file, a.MAIL);
+	if (mail == 1)
+	{
+		getInfo(file, a.MAIL);
+	}
 	getInfo(file, a.NS);
 	getInfo(file, a.HA);
 	getInfo(file, a.MT);
 	getHobby(file, a.ST, soST);
 }
 
-void optional()
+void optional(int &hvt, int &mssv, int &khoa, int &khoas, int &mail, int &ngaysinh, int &mota, int &sothich, int &mailtemp)
 {
-
+	wprintf(L"Lựa chọn các trường nội dung bạn muốn hiển thị (chọn gõ \"1\", không chọn gõ \"0\"):\n");
+	wprintf(L"1. Họ và tên: ");
+	wscanf(L"%d", &hvt);
+	wprintf(L"2. Mã số sinh viên: ");
+	wscanf(L"%d", &mssv);
+	wprintf(L"3. Khoa: ");
+	wscanf(L"%d", &khoa);
+	wprintf(L"4. Khóa tuyển: ");
+	wscanf(L"%d", &khoas);
+	wprintf(L"5. Ngày sinh: ");
+	wscanf(L"%d", &ngaysinh);
+	wprintf(L"6. Mô tả bản thân: ");
+	wscanf(L"%d", &mota);
+	wprintf(L"7. Sở thích: ");
+	wscanf(L"%d", &sothich);
+	if (mail == 1)
+	{
+		wprintf(L"8. Mail: ");
+		wscanf(L"%d", &mailtemp);
+	}
 }
 
-void taoFileHtm(SV a, int soST)
+void taoFileHtm(SV a, int soST, int hvt, int mssv, int khoa, int khoas, int mail, int ngaysinh, int mota, int sothich)
 {
 	wchar_t ten[30];
 	int i, j;
@@ -176,30 +198,57 @@ void taoFileHtm(SV a, int soST)
 		fwprintf(f, L"\t\t\t\t<div class=\"MainContain\">\n");
 		fwprintf(f, L"\t\t\t\t\t<!-- Begin Top Region -->\n");
 		fwprintf(f, L"\t\t\t\t\t<div class=\"MainContain_Top\">\n");
-		fwprintf(f, L"\t\t\t\t\t\t<div class=\"InfoGroup\">Thông tin cá nhân</div>\n");
-		fwprintf(f, L"\t\t\t\t\t\t<div>\n");
-		fwprintf(f, L"\t\t\t\t\t\t\t<ul class=\"TextInList\">\n");
-		fwprintf(f, L"\t\t\t\t\t\t\t\t<li>Họ và tên: %ls</li>\n", a.HVT);
-		fwprintf(f, L"\t\t\t\t\t\t\t\t<li>MSSV: %ls</li>\n", a.MSSV);
-		fwprintf(f, L"\t\t\t\t\t\t\t\t<li>Sinh viên khoa %ls</li>\n", a.KHOA);
-		fwprintf(f, L"\t\t\t\t\t\t\t\t<li>Khóa tuyển: %d</li>\n", a.KHOAS);
-		fwprintf(f, L"\t\t\t\t\t\t\t\t<li>Ngày sinh: %ls</li>\n", a.NS);
-		fwprintf(f, L"\t\t\t\t\t\t\t\t<li>Email: %ls</li>\n", a.MAIL);
-		fwprintf(f, L"\t\t\t\t\t\t\t</ul>\n");
-		fwprintf(f, L"\t\t\t\t\t\t</div>\n");
-		fwprintf(f, L"\t\t\t\t\t\t<div class=\"InfoGroup\">Sở thích</div>\n");
-		fwprintf(f, L"\t\t\t\t\t\t<div>\n");
-		fwprintf(f, L"\t\t\t\t\t\t\t<ul class=\"TextInList\">\n");
-		for (int i = 0; i <= soST; i++)
+		if (hvt == 1 || mssv == 1 || khoa == 1 || khoas == 1 || ngaysinh == 1 || mail == 1)
 		{
-			fwprintf(f, L"\t\t\t\t\t\t\t\t<li>%ls</li>\n", *(a.ST + i));
+			fwprintf(f, L"\t\t\t\t\t\t<div class=\"InfoGroup\">Thông tin cá nhân</div>\n");
+			fwprintf(f, L"\t\t\t\t\t\t<div>\n");
+			fwprintf(f, L"\t\t\t\t\t\t\t<ul class=\"TextInList\">\n");
+			if (hvt == 1)
+			{
+				fwprintf(f, L"\t\t\t\t\t\t\t\t<li>Họ và tên: %ls</li>\n", a.HVT);
+			}
+			if (mssv == 1)
+			{
+				fwprintf(f, L"\t\t\t\t\t\t\t\t<li>MSSV: %ls</li>\n", a.MSSV);
+			}
+			if (khoa == 1)
+			{
+				fwprintf(f, L"\t\t\t\t\t\t\t\t<li>Sinh viên khoa %ls</li>\n", a.KHOA);
+			}
+			if (khoas == 1)
+			{
+				fwprintf(f, L"\t\t\t\t\t\t\t\t<li>Khóa tuyển: %d</li>\n", a.KHOAS);
+			}
+			if (ngaysinh == 1)
+			{
+				fwprintf(f, L"\t\t\t\t\t\t\t\t<li>Ngày sinh: %ls</li>\n", a.NS);
+			}
+			if (mail == 1)
+			{
+				fwprintf(f, L"\t\t\t\t\t\t\t\t<li>Email: %ls</li>\n", a.MAIL);
+			}
 		}
 		fwprintf(f, L"\t\t\t\t\t\t\t</ul>\n");
 		fwprintf(f, L"\t\t\t\t\t\t</div>\n");
-		fwprintf(f, L"\t\t\t\t\t\t<div class=\"InfoGroup\">Mô tả</div>\n");
-		fwprintf(f, L"\t\t\t\t\t\t<div class=\"Description\">\n");
-		fwprintf(f, L"\t\t\t\t\t\t\t%ls.\n", a.MT);
-		fwprintf(f, L"\t\t\t\t\t\t</div>\n");
+		if (sothich == 1)
+		{
+			fwprintf(f, L"\t\t\t\t\t\t<div class=\"InfoGroup\">Sở thích</div>\n");
+			fwprintf(f, L"\t\t\t\t\t\t<div>\n");
+			fwprintf(f, L"\t\t\t\t\t\t\t<ul class=\"TextInList\">\n");
+			for (int i = 0; i <= soST; i++)
+			{
+				fwprintf(f, L"\t\t\t\t\t\t\t\t<li>%ls</li>\n", *(a.ST + i));
+			}
+			fwprintf(f, L"\t\t\t\t\t\t\t</ul>\n");
+			fwprintf(f, L"\t\t\t\t\t\t</div>\n");
+		}
+		if (mota == 1)
+		{
+			fwprintf(f, L"\t\t\t\t\t\t<div class=\"InfoGroup\">Mô tả</div>\n");
+			fwprintf(f, L"\t\t\t\t\t\t<div class=\"Description\">\n");
+			fwprintf(f, L"\t\t\t\t\t\t\t%ls.\n", a.MT);
+			fwprintf(f, L"\t\t\t\t\t\t</div>\n");
+		}
 		fwprintf(f, L"\t\t\t\t\t</div>\n");
 		fwprintf(f, L"\t\t\t\t</div>\n");
 		fwprintf(f, L"\t\t\t</div>\n");
@@ -232,10 +281,24 @@ void wmain(int argc, wchar_t* argv[])
 {
 	_setmode(_fileno(stdout), _O_U16TEXT);
 	_setmode(_fileno(stdin), _O_U16TEXT);
+	int hvt = 1;
+	int mssv = 1;
+	int khoa = 1;
+	int khoas = 1;
+	int mail = 1;
+	int mailtemp = 1;
+	int ngaysinh = 1;
+	int mota = 1;
+	int sothich = 1;
 	SV a[100];
 	int soST;
 	int soSV = 0;
-	FILE* f = _wfopen(L"thongtin.csv", L"r,ccs=UTF-8");
+	int option = 1;
+	wchar_t ten[30];
+	wprintf(L"Sao chép tập tin dữ liệu của bạn vào thư mục của project này. \nSau đó nhập tên của tập tin csv (ví dụ: thongtin.csv, là tập tin sẵn của sinh viên làm đồ án): ");
+	wscanf(L"%ls", ten);
+	wprintf(L"%ls", ten);
+	FILE* f = _wfopen(ten, L"r,ccs=UTF-8");
 	if (f != NULL)
 	{
 		while (!feof(f))
@@ -247,14 +310,30 @@ void wmain(int argc, wchar_t* argv[])
 			}
 		}
 		fseek(f, 3, SEEK_SET);
-		for (int i = 0; i < soSV; i++)
+		wprintf(L"Tập tin thông tin của bạn có trường email không?\nNếu có gõ \"1\", nếu không có gõ \"0\" (Tập tin mặc định của sinh viên nhập \"1\"): ");
+		wscanf(L"%d", &mail);
+		wprintf(L"Hỗ trợ cấu hình trang: Bạn muốn tùy chỉnh cho tất cả các trang nhập \"1\", tùy chỉnh cho từng trang nhập \"0\":");
+		wscanf(L"%d", &option);
+		if (option == 1)
 		{
-			wprintf(L"Sinh viên thứ %d: ", i + 1);
-			layDuLieu(f, *(a + i), soST);
-			taoFileHtm(*(a + i), soST);
-			int vitri = ftell(f);
-			wprintf(L"Kết thúc sinh viên %d. Vị trí con trỏ file: %d\n", i + 1, vitri);
+			optional(hvt, mssv, khoa, khoas, mail, ngaysinh, mota, sothich, mailtemp);
+			for (int i = 0; i < soSV; i++)
+			{
+				layDuLieu(f, *(a + i), soST, mail);
+				taoFileHtm(*(a + i), soST, hvt, mssv, khoa, khoas, mailtemp, ngaysinh, mota, sothich);
+			}
 		}
+		else
+		{
+			for (int i = 0; i < soSV; i++)
+			{
+				wprintf(L"Sinh viên %d:\n", i + 1);
+				layDuLieu(f, *(a + i), soST, mail);
+				optional(hvt, mssv, khoa, khoas, mail, ngaysinh, mota, sothich, mailtemp);
+				taoFileHtm(*(a + i), soST, hvt, mssv, khoa, khoas, mailtemp, ngaysinh, mota, sothich);
+			}
+		}
+		wprintf(L"Đã phát sinh tất cả các trang theo yêu cầu!\n");
 		fclose(f);
 	}
 	else
